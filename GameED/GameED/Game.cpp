ea6e4072d54c1feb0dbcf8ac299f74adcdc83516
Leandro P. Novak <leandro.novak@outlook.com>
2016-04-14 15:08:49 -0300

    Update Game.cpp
    Comentado

--- GameED/GameED/Game.cpp
+++ GameED/GameED/Game.cpp
@@ -1,63 +1,74 @@
+/*****************************************
+ * Game.cpp
+ * Classe Game, possui o gameLoop do jogo
+******************************************/
 #include "Game.h"
 
+// Construtor da classe Game
+// Recebe por parâmetro sf::VideoMode com largura e altura da janela de jogo,
+// uma string contendo o nome da janela e sf::Style o estilo da janela.
 
 Game::Game() : mainWindow(sf::VideoMode(800, 600), "EDGame", sf::Style::Close)
 {
-	mainWindow.setFramerateLimit(30);
-	mainWindow.setVerticalSyncEnabled(false);
-	currentState = State::menu;
+	mainWindow.setFramerateLimit(30);				// Força a taxa de frames limite do jogo
+	mainWindow.setVerticalSyncEnabled(false);			// Desativa a sincronização Vertical
+	currentState = State::menu;					
 
-	if (!tLayout.loadFromFile("Content/layout.png"))
+	if (!tLayout.loadFromFile("Content/layout.png"))		
 		std::cout << "Erro ao carregar layout" << std::endl;
 	else
 	{ 
-		sLayout.setTexture(tLayout);
-		sLayout.setPosition(sf::Vector2f(0.f, 0.f));
+		sLayout.setTexture(tLayout);				
+		sLayout.setPosition(sf::Vector2f(0.f, 0.f));		
 	}
 
 	block.load("1.png", 3, 388);
 }
 
-
+// Destrutor da classe Game
 Game::~Game()
 {
 }
 
+// Loop principal do jogo
 void Game::run()
 {
-	sf::Clock clock;
-	sf::Time timeSinceLastUpdate = sf::Time::Zero;
-	sf::Time gameTime = sf::seconds(1.f / 60.f);
+	sf::Clock clock;						// Contador de tempo do jogo
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;			// Variável para tempo passado da ultima atualização
+	sf::Time gameTime = sf::seconds(1.f / 30.f);			// Espaço de tempo entre atualizações 1/30 segundos (30fps)
 
-	initialize();
+	initialize();							
 
 	while (mainWindow.isOpen())
 	{
-		eventHandler();
-		timeSinceLastUpdate += clock.restart();
+		eventHandler();						// Gerenciador de eventos
+		timeSinceLastUpdate += clock.restart();			// Tempo desde a ultima atualização do jogo
 		
-		while (timeSinceLastUpdate > gameTime)
+		while (timeSinceLastUpdate > gameTime)			// Verifica se o tempo passado é superior a 1/30 segundos
 		{
-			timeSinceLastUpdate -= gameTime;
-			eventHandler();
+			timeSinceLastUpdate -= gameTime;		// Decrementa o tempo da atualização
+			eventHandler();					// Gerenciador de eventos
 		}
 
-		update(gameTime.asSeconds());
-		draw();
+		update(gameTime.asSeconds());			
+		draw();							
 	}
 }
 
+// Inicializador de conteúdo secundário
 void Game::initialize()
 {
 
 }
 
+// Gerenciador de eventos
 void Game::eventHandler()
 {
 	sf::Event event;
 	
 	while (mainWindow.pollEvent(event))
 	{
+		// Eventos verificados no menu do jogo
 		if (currentState == State::menu)
 		{
 			switch (event.type)
@@ -69,7 +80,8 @@ void Game::eventHandler()
 
 			block.eventHandler(mainWindow, event);
 		}
-
+		
+		// Eventos verificados durante a fase
 		if (currentState == State::playing)
 		{
 			switch (event.type)
@@ -82,21 +94,26 @@ void Game::eventHandler()
 	}
 }
 
+// Atualiza conteúdo do jogo
 void Game::update(float timeAsSeconds)
 {
+	// Atualização no menu
 	if (currentState == State::menu)
 	{
 		block.update(timeAsSeconds, mainWindow);
 	}
 
+	// Atualização durante a fase
 	if (currentState == State::playing)
 	{
 
 	}
 }
 
+// Exibe conteúdo na janela do jogo
 void Game::draw()
 {
+	// Conteúdo a ser desenhado durante o menu
 	mainWindow.clear(sf::Color::Magenta);
 	if (currentState == State::menu)
 	{
@@ -104,6 +121,7 @@ void Game::draw()
 		mainWindow.draw(sLayout);
 	}
 
+	// Conteúdo a ser desenhado durante a fase
 	if (currentState == State::playing)
 	{
 

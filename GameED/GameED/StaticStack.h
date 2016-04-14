84492a2d84b52682b0572bcadd859f514ea43540
Leandro P. Novak <leandro.novak@outlook.com>
2016-04-14 15:21:15 -0300

    Comentado

--- GameED/GameED/StaticStack.h
+++ GameED/GameED/StaticStack.h
@@ -1,26 +1,27 @@
 #pragma once
 #define DEFAULTSIZE 10;
 
+// Classe StaticStack
 template <class T>
 class StaticStack
 {
 public:
-	StaticStack();
-	StaticStack(int);
+	StaticStack();				// Construtor padrão
+	StaticStack(int);			// Construtor com tamanho definido pelo usuário
 
-	~StaticStack();
+	~StaticStack();				// Destrutor de pilha
 
-	bool push(T);
-	T pop();
+	bool push(T);				// Push
+	T pop();				// Pop
 
-	bool isFull();
-	bool isEmpty();
+	bool isFull();				// Verifica se a pilha está cheia
+	bool isEmpty();				// Verifica se a pilha está vazia
 
-	T getTop();
+	T getTop();				// Retorna item do topo da pilha
 
 private:
-	T * node;
-	int top;
-	int max;
+	T * node;				// Ponteiro do tipo pilha
+	int top;				// Índice do topo da pilha
+	int max;				// Tamanho máximo da pilha
 };
 

41983849568735e7823acf8a8db682adbdaeb630
Leandro P. Novak <leandro.novak@outlook.com>
2016-04-14 15:26:37 -0300

    Comentado

--- GameED/GameED/movableSprite.h
+++ GameED/GameED/movableSprite.h
@@ -3,25 +3,29 @@
 #include <string>
 #include <iostream>
 
+/******************************************************************
+* Classe MovableSprite
+* Objeto que representa basicamente uma imagem que pode ser movida
+*******************************************************************/
 class movableSprite
 {
 	public:
-		movableSprite();
-		~movableSprite();
+		movableSprite();					// Construtor
+		~movableSprite();					// Destrutor
 
-		void load(std::string filename, int x, int y);
-		void eventHandler(sf::RenderWindow &, sf::Event &);
-		void update(float, sf::RenderWindow &);
-		void draw(sf::RenderTarget &, sf::RenderStates);
+		void load(std::string filename, int x, int y);		// Carrega imagem em arquivo e atribui posição da imagem
+		void eventHandler(sf::RenderWindow &, sf::Event &);	// Verifica eventos relacionados à imagem
+		void update(float, sf::RenderWindow &);			// Atualiza posição da imagem
+		void draw(sf::RenderTarget &, sf::RenderStates);	// Desenha imagem na janela
 
 	private:
-		std::string defaultLocation;
-		sf::Texture _texture;
-		sf::Sprite _sprite;
+		std::string defaultLocation;				// Localização padrão das imagens
+		sf::Texture _texture;					// Texture da imagem
+		sf::Sprite _sprite;					// Sprite da imagem
 
-		sf::Vector2f _vector2f;
-		sf::Vector2i _vector2i;
+		sf::Vector2f _vector2f;					// Posição float da imagem
+		sf::Vector2i _vector2i;					// Posição int da imagem
 
-		bool mouseIsPressed;
+		bool mouseIsPressed;					// Status do botão direito do mouse
 };
 
